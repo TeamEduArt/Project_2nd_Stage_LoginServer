4c070365ec1bd96d931b47d3c91dc6ad7237efd0
NHoose <Nikolas.Hoose@gmail.com>
2016-01-13 21:51:11 +0100

    new functions

--- node-android/routes/routes.js
+++ node-android/routes/routes.js
@@ -1,4 +1,6 @@
 var testlogin = require('config/testlogin');
+var appsperr = require('config/appsperr');
+var filesend = require('config/filesend');
  
  
 module.exports = function(app) {
@@ -20,6 +22,26 @@ module.exports = function(app) {
             res.json(found);
 
         }); 
+    });
+    
+    app.post('/appsperr', function(req,res){
+        var userid = req.body.userid;
+        var operation = req.body.operation;
+        
+        appsperr.appsperr(userid, operation, function (found) {
+            console.log(found);
+            res.json(found);
+        });
+    });
+    
+    app.post('/filesend', function(req,res){
+        var userid = req.body.userid;
+        var operation = req.body.operation;
+        
+        filesend.filesend(userid, operation, function (found) {
+            console.log(found);
+            res.json(found);
+        });
     });
 	
 };
